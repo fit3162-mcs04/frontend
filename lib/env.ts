dd3218e1edb7917e8232e4792439d0486943d7b3
haseakito <haseakito@gmail.com>
2025-04-23 13:06:58 +0900

    feat: add utility functions

---
+++ lib/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod"
+
+const envVariables = z.object({
+  NEXT_PUBLIC_APP_URL: z.string(),
+  BETTER_AUTH_URL: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+})
+
+envVariables.parse(process.env)
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
